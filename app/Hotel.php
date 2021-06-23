cc92951a1347497b21bd7bc3810f0920f9cb85a5
umutbaris <umutbariskarasar@outlook.com>
2021-06-23 22:24:24 +0300

    Adding Hotel Model And Migration

---
+++ app/Hotel.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Hotel extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'category_id',
+        'name',
+        'rating',
+        'reputation',
+        'reputationBadge',
+        'availability',
+        'price',
+        'image',
+    ];
+
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+
+    /**
+     * Get the attributes for the hotels.
+     */
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    /**
+     * Get the location associated with the hotel.
+     */
+    public function location()
+    {
+        return $this->hasOne(Location::class);
+    }
+
+}
---
+++ database/migrations/2021_06_23_181939_create_hotels_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHotelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hotels', function (Blueprint $table) {
+            $table->increments ('id');
+            $table->integer ('category_id')->unsigned();
+            $table->string('name');
+            $table->integer('rating');
+            $table->integer('reputation');
+            $table->integer('reputationBadge');
+            $table->integer('availability');
+            $table->integer('price');
+            $table->string('image');
+
+            $table->foreign('category_id')
+                ->references('id')
+                ->on('categories');
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hotels');
+    }
+}
