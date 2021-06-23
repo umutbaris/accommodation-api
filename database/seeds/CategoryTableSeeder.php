a24d1816fae3c242e65e19006226ccf215184786
umut <umutbariskarasar@gitlab.com>
2021-06-23 20:52:16 +0300

    Adding category model, migration and seeder

---
+++ database/seeds/CategoryTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Category;
+
+class CategoryTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $categories = [
+            "Hotel", "Alternative", "Hostel", "Lodge", "Resort", "Guest House"
+        ];
+
+        foreach ($categories as $category) {
+            Category::create(
+                [
+                    "name" => $category,
+                    "slug" => Str::slug($category, "-")
+                ]
+            );
+        }
+    }
+}
