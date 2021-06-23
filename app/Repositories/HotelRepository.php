19f7bcbfbdca65e6c5754aa67d628023111b6294
umutbaris <umutbariskarasar@outlook.com>
2021-06-23 23:43:50 +0300

    Adding Custom Store Method To Calculate Reputation Badge In Repository

--- app/Repositories/HotelRepository.php
+++ app/Repositories/HotelRepository.php
@@ -8,4 +8,36 @@ use App\Hotel;
 class HotelRepository extends BaseRepository
 {
     protected $modelName = Hotel::class;
+
+    /**
+     * @param  array  $data
+     * @return mixed
+     */
+    public function store($data) {
+        $data['reputationBadge'] = $this->calculateReputationBadge($data['reputation']);
+        $hotel = parent::store($data);
+
+        return $hotel;
+    }
+    
+    /**
+     * The reputation badge is a calculated value that depends on the reputation
+     *
+     * @todo Calculation part can change using like map according to range to make generic
+     * 
+     * @param  Integer  $reputation
+     * @return string
+     */
+    public function calculateReputationBadge(int $reputation) {
+        $reputationBadge = 'green';
+        if($reputation <= 799) {
+            $reputationBadge = 'yellow';
+        }
+
+        if($reputation <= 500) {
+            $reputationBadge = 'red';
+        }
+
+        return $reputationBadge;
+    }
 }
