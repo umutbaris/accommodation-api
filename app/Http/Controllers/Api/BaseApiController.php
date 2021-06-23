5f984cad6eb73b335a6927008ff4de70240bc257
umut <umutbariskarasar@gitlab.com>
2021-06-23 20:19:59 +0300

    Implementing laravel mysql docker skeleton

---
+++ app/Http/Controllers/Api/BaseApiController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+use App\Repositories\BaseRepository;
+
+class BaseApiController extends Controller
+{
+    protected $baseRepository;
+
+    public function __construct(BaseRepository $baseRepository)
+    {
+        $this->BaseRepository = $baseRepository;
+    }
+
+    public function sendRaw($data = [], $statusCode = 200) {
+        return response()->json($data, $statusCode);
+    }
+
+    public function sendSuccess($data = [], $statusCode = 200) {
+        return response()->json([
+            'success' => true,
+            'data' => $data
+        ], $statusCode);
+    }
+
+    public function sendError($error, $statusCode = 200, $errorType='') {
+        $response = [
+            'success' => false,
+            'error' => $error
+        ];
+
+        if(!empty($errorType)){
+            $response['errorType'] = $errorType;
+        }
+        return response()->json($response, $statusCode);
+    }
+
+    public function sendResponse($result) {
+        if(is_array($result) && array_key_exists('Error', $result)) {
+            return $this->sendError($result);
+        }
+
+        return $this->sendSuccess($result);
+    }
+
+    public function logActivity($description, $instance = null, $properties = [])
+    {
+        if ($instance!= null) $instance = $instance->getNewInstance();
+        activity()
+//            ->on($instance)
+            ->withProperties($properties)
+            ->log($description);
+    }
+}
---
+++ app/Repositories/UserRepository.php
@@ -0,0 +1,7 @@
+<?php
+namespace App\Repositories;
+use App\User;
+
+class UserRepository extends BaseRepository {
+    protected $modelName = User::class;
+}
